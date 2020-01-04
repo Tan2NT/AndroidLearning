bca4f6a35da16a453d282f263b50753621680d98
tan <hoangngoctan17@gmail.com>
2020-01-04 10:58:34 +0700

    * add project files

---
+++ Android/Intent/app/src/main/java/com/example/intent/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.intent
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    final var TAG : String = "TDebug"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        switchToMainActivity()
+        Log.i(TAG, "----- onCreate -----")
+    }
+
+    private fun switchToMainActivity(){
+        btnMainActivity.setOnClickListener(View.OnClickListener {
+            var secondIntent : Intent = Intent(this, SecondActivity::class.java)
+            startActivity(secondIntent)
+
+        })
+    }
+
+    override fun onStart() {
+        Log.i(TAG, "----- onStart -----")
+        super.onStart()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "----- onPause -----")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "----- onStop -----")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "----- onDestroy -----")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "----- onRestart -----")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "----- onResume -----")
+    }
+}
