57566321a7e1c7e38c4ef1ec611d4645568dc211
tan <hoangngoctan17@gmail.com>
2020-05-03 21:40:27 +0700

    * Lesson 5: Test double and dependency injection (not complete)

--- KotlinLearning/KotlinAdvanced/DependenciesInjectionAndTesting/TestingBasics/NotesApp/app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ KotlinLearning/KotlinAdvanced/DependenciesInjectionAndTesting/TestingBasics/NotesApp/app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -3,6 +3,8 @@ package com.example.android.architecture.blueprints.todoapp.tasks
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.source.FakeTestRepository
 import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.*
@@ -11,15 +13,24 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
 
     // Subject under test
     private lateinit var tasksViewModel: TasksViewModel
 
+    // Use a fake repository to be injected into the view
+    private lateinit var tasksRepository: FakeTestRepository
+
     @Before
     fun setupViewModel() {
-        tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+        // We initialize the tasks to 3, with one active
+        tasksRepository = FakeTestRepository()
+        val task1 = Task("Title1", "Des 1")
+        val task2 = Task("Title2", "Des 2", true)
+        val task3 = Task("Title3", "Des 3", true)
+        tasksRepository.addTasks(task1, task2, task3)
+
+        tasksViewModel = TasksViewModel(tasksRepository)
     }
 
     @get:Rule
